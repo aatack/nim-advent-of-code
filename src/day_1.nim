82135f7de3abc464691be6d8c4f78289790fc10f
aatack <alexanderjtatack@gmail.com>
2021-12-03 01:23:04 +0000

    Refactor the code a little

--- src/day_1.nim
+++ src/day_1.nim
@@ -1,17 +1,20 @@
-import strutils
+import strutils, sequtils
 
 iterator parseInts(inputData: string): int =
   for line in inputData.split(Newlines):
     if line.len > 0:
       yield parseInt(line)
 
-func depthMeasurementIncreases*(inputData: string): int =
+func countIncreases(values: seq[int]): int =
   var
     previous: int
     initialised: bool = false
 
-  for current in parseInts(inputData):
+  for current in values:
     if initialised and (current > previous):
       inc result
     initialised = true
     previous = current
+
+func depthMeasurementIncreases*(inputData: string): int =
+  countIncreases(toSeq(parseInts(inputData)))
