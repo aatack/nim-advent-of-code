583a98f9a70ec4a411b6586d9af6c4cefaff4d5a
aatack <alexanderjtatack@gmail.com>
2021-12-03 01:39:44 +0000

    Implement a simple buffer type

--- src/day_1.nim
+++ src/day_1.nim
@@ -1,5 +1,20 @@
 import strutils, sequtils
 
+type
+  UnorderedBuffer*[I: static[int], T] = ref object
+    values: array[I, T]
+    current: int
+
+method store*[I, T](this: UnorderedBuffer[I, T], value: T): void {.base.} =
+  this.values[this.current] = value
+  inc this.current
+  if this.current == I:
+    this.current = 0
+
+method get*[I, T](this: UnorderedBuffer[I, T]): array[I, T] {.base.} =
+  let copy = this.values
+  result = copy
+
 iterator parseInts(inputData: string): int =
   for line in inputData.split(Newlines):
     if line.len > 0:
