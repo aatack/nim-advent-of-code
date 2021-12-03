6d27875ac4c924557e957abf06d09a85a2a37130
aatack <alexanderjtatack@gmail.com>
2021-12-03 18:42:49 +0000

    Lay out the data structures for day 2

--- src/day_02.nim
+++ src/day_02.nim
@@ -1,2 +1,37 @@
-func partOne*(data: string): int =
+import strutils
+
+type
+  Direction = enum
+    forward, down, up
+  Command = object
+    direction: Direction
+    magnitude: int
+
+func parseCommand(line: string): Command =
+  let
+    segments = line.split
+  assert segments.len == 2
+  return Command(
+    direction: (
+      case segments[0]:
+        of "forward":
+          forward
+        of "down":
+          down
+        of "up":
+          up
+        else:
+          raise newException(Exception, "Invalid direction: " & segments[0])
+    ),
+    magnitude: parseInt(segments[1])
+  )
+
+iterator parseData*(data: string): Command =
+  for line in data.splitLines:
+    yield parseCommand(line)
+
+proc partOne*(data: string): int =
+  # TODO: change back to func
+  for command in parseData(data):
+    discard
   0
