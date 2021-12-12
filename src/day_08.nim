103a91f6b1712aa604e3187d71e720f6b09e6ab0
aatack <alexanderjtatack@gmail.com>
2021-12-12 23:36:54 +0000

    Implement and test code for converting the final result

--- src/day_08.nim
+++ src/day_08.nim
@@ -8,6 +8,20 @@ type
     potential: array[10, Reading]
     displayed: array[4, Reading]
 
+const
+  originalReadings = [
+    {a, b, c, e, f, g},
+    {c, f},
+    {a, c, d, e, g},
+    {a, c, d, f, g},
+    {b, c, d, f},
+    {a, b, d, f, g},
+    {a, b, d, e, f, g},
+    {a, c, f},
+    {a, b, c, d, e, f, g},
+    {a, b, c, d, f, g},
+  ]
+
 func parseReading(data: string): Reading =
   for character in data:
     result.incl(parseEnum[Segment]($character))
@@ -21,6 +35,38 @@ proc parseDisplay(data: string): Display =
     result.potential[i] = reading
   for i, reading in parts[1].splitWhitespace.map(parseReading):
     result.displayed[i] = reading
+
+func getSegmentMapping(display: Display): array[Segment, Segment] =
+  # Return a mapping of displayed segments to actual segments
+  result[d] = a
+  result[e] = b
+  result[a] = c
+  result[f] = d
+  result[g] = e
+  result[b] = f
+  result[c] = g
+
+func readingToInt(reading: Reading): int =
+  for i, r in originalReadings:
+    if r == reading:
+      return i
+
+  raise newException(Exception, "Unrecognised reading: " & $reading)
+
+func displayToInt(display: Display): int =
+  let
+    mapping = getSegmentMapping(display)
+  var
+    stringReading = ""
+
+  for reading in display.displayed:
+    var
+      mappedReading: Reading = {}
+    for segment in reading:
+      mappedReading.incl(mapping[segment])
+    stringReading = stringReading & $readingToInt(mappedReading)
+
+  return parseInt(stringReading)
 
 func partOne*(data: string): int =
   const
@@ -38,3 +84,7 @@ func partOne*(data: string): int =
         inc count
 
   return count
+
+proc partTwo*(data: string): int =
+  let
+    displays = data.splitLines.map(parseDisplay)
