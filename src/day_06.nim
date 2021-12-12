346fc1306a9ecffc249883593aa2435e29fcc3bc
aatack <alexanderjtatack@gmail.com>
2021-12-12 01:07:42 +0000

    Add a new type for tracking fish in aggregate

--- src/day_06.nim
+++ src/day_06.nim
@@ -1,16 +1,22 @@
 import options, sequtils, strutils, sugar
 
+const
+  normalGestation = 6
+  newbornGestation = 8
+
 type
   Lanternfish = ref object
     days: int
   School = ref object
     fish: seq[Lanternfish]
+  IndexedSchool = ref object
+    fish: array[newbornGestation + 1, int]
 
 method loop(this: Lanternfish): Option[Lanternfish] {.base.} =
   dec this.days
   if this.days < 0:
-    this.days = 6
-    return some(Lanternfish(days: 8))
+    this.days = normalGestation
+    return some(Lanternfish(days: newbornGestation))
 
 method loop(this: School) {.base.} =
   var
@@ -19,6 +25,14 @@ method loop(this: School) {.base.} =
   for fish in newborns:
     if fish.isSome:
       this.fish.add(fish.get)
+
+method loop(this: IndexedSchool) =
+  let
+    due = this.fish[0] # Fish that are about to give birth
+  for i in 0..(newbornGestation - 1):
+    this.fish[i] = this.fish[i + 1]
+  this.fish[newbornGestation] = due
+  this.fish[normalGestation] = due
 
 func parseSchool(data: string): School =
   return School(
