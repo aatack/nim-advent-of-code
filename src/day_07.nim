c6a236054d5c593b3223fc0003c1f67cde28da66
aatack <alexanderjtatack@gmail.com>
2021-12-12 01:34:46 +0000

    Update cost function

--- src/day_07.nim
+++ src/day_07.nim
@@ -8,6 +8,17 @@ func cost(positions: seq[int], position: int): int =
     total += abs(p - position)
   return total
 
+func partTwoCost(positions: seq[int], position: int): int =
+  var
+    total = 0
+
+  func fuel(distance: int): int =
+    return ((distance * distance) + distance) div 2
+
+  for p in positions:
+    total += fuel(abs(p - position))
+  return total
+
 func partOne*(data: string): int =
   let
     positions = data.split(',').map(parseInt)
@@ -18,6 +29,21 @@ func partOne*(data: string): int =
   for position in 0..positions.max:
     let
       positionCost = cost(positions, position)
+    if positionCost < currentMinimumCost:
+      currentMinimumCost = positionCost
+
+  return currentMinimumCost
+
+func partTwo*(data: string): int =
+  let
+    positions = data.split(',').map(parseInt)
+
+  var
+    currentMinimumCost = high(int)
+  
+  for position in 0..positions.max:
+    let
+      positionCost = partTwoCost(positions, position)
     if positionCost < currentMinimumCost:
       currentMinimumCost = positionCost
 
